5c5f82b309902607ec5fae58a021b695ccc71f82
m1k431 <garcia.mikaelgmail.com>
2020-11-09 13:12:56 +0100

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,38 @@
-import React from 'react'
+/* eslint-disable react/react-in-jsx-scope */
 import Header from '../components/Header'
 import Jonglage from '../components/Jonglage'
 import JeuCv from '../components/JeuCv'
 import Soleil from '../components/soleil'
 import Particles from 'react-particles-js'
 
-const bienvenue = 'BIENVENUE DANS MON LABO DE DEV',
+export default function Home({
   welcome = 'WELCOME TO MY DEVLAB',
-  moiFR = 'JE SUIS MIKAEL',
   moiUS = 'MY NAME IS MIKAEL',
-  metierFR = 'INGENIEUR LOGICIEL IBM',
   metierUS = 'I AM AN IBM SOFTWARE ENGINEER',
-  intro = 'THIS WEBSITE HOSTS MY EXPERIENCES AND SKILLS'
-
-const paramParticles = {
-  fpsLimit: 60,
-  backgroundMode: {
-    enable: true,
-  },
-  "particles": {
-    "number": {
-      "value": 80
+  intro = 'THIS WEBSITE HOSTS MY EXPERIENCES AND SKILLS',
+  paramParticles = {
+    fpsLimit: 60,
+    backgroundMode: {
+      enable: true,
     },
-    "size": {
-      "value": 1
-    }
-  },
-  "interactivity": {
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
+    "particles": {
+      "number": {
+        "value": 80
+      },
+      "size": {
+        "value": 1
+      }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        }
       }
     }
   }
-}
-
-function Home() {
+}) {
   return (
     <div>
       <Header />
@@ -55,4 +51,19 @@ function Home() {
   )
 }
 
-export default Home
+export const getStaticProps = async () => {
+  // This is a full server-side Node environment,
+  // which means that you can make network requests,
+  // talk to databases, read from the file-system,
+  // and do whatever you want to fetch your data.
+
+  return {
+    props: {
+      title: "My Amazing Startup",
+      metaContent: "Amazing SEO poetry",
+      copy:
+        "I'm in the business of making people smile. That's all I'm here for.",
+    },
+    revalidate: 1,
+  }
+}
