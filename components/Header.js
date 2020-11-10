e86e44fe4e9456b8ff2d3a8a12aaedef7b9d8635
m1k431 <garcia.mikaelgmail.com>
2020-11-10 15:47:24 +0100

    w3c validation title + lang

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ import linkedIn from '../public/img/LinkedIn_Logo.svg';
 class Header extends Component {
   render() {
     const currentURL = "https://www.mikael.ml",
-    previewImage = "https://nextfolio.vercel.app/ogimage.png"
+      previewImage = "https://nextfolio.vercel.app/ogimage.png"
     return (
       <div>
         <Head>
@@ -17,12 +17,13 @@ class Header extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=0.7" />
           <meta name="theme-color" content="#000000" />
           <meta name="description" content="My Javascript Portfolio" />
-          <meta name="title" content="My Javascript Portfolio" />
           <meta property="og:url" content={currentURL} key="ogurl" />
           <meta property="og:image" content={previewImage} key="ogimage" />
           <meta property="og:site_name" content="m1k431" key="ogsitename" />
           <meta property="og:title" content="DEV Lab" key="ogtitle" />
           <meta property="og:description" content="JS portfolio" key="ogdesc" />
+          <title>My Javascript Portfolio</title>
+          <html lang="fr" />
         </Head>
 
         <Navbar collapseOnSelect expand="sm" bg="blue" variant="dark">
