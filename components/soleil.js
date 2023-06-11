6cd901dbb81ed8b06fbb7f00890fc5b902e06865
m1k431 <garcia.mikael@gmail.com>
2023-06-11 17:06:32 +0200

    test border radius webkit

--- components/soleil.js
+++ components/soleil.js
@@ -363,8 +363,9 @@ class soleil extends Component {
         this.div.style.marginLeft = "auto"
         this.div.style.marginRight = "auto"
         this.div.style.marginBottom = "2%"
-        this.div.style.borderRadius = "50%"
+        //this.div.style.borderRadius = "50%"
         this.div.style.webkitBorderRadius = '50%'
+        //this.div.style.webkitAppearance = 'textfield'
 
         //Création de l'environnement 
         this.ciel = new Ciel()
